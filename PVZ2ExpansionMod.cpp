56d70f2c8dd18e9e8c42ed7cb6ff183cc5d1e4c6
Fga721 <felipetshopempresa@gmail.com>
2025-11-06 17:55:29 -0300

    Add files via upload

---
+++ PVZ2ExpansionMod.cpp
@@ -0,0 +1,230 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unistd.h>
+
+#include "PVZ2ExpansionMod.h"
+
+#include <Sexy/SexyApp.h>
+
+#include "PvZ2/Board.h"
+#include "PvZ2/PlantType.h"
+#include "PvZ2/WorldMap.h"
+#include "PvZ2/ZombieType.h"
+
+#include "PvZ2/Plant_PowerLily.h"
+#include "PvZ2/Zombie_EightiesArcade.h"
+#include "PvZ2/Zombie_Camel.h"
+
+
+#include <PvZ2/Zombie_Bull.h>
+#include <PvZ2/Chairracer.h>
+#include <PvZ2/Zombie_BullVeteran.h>
+
+
+
+#pragma region Alias to ID
+
+class ZombieAlmanac
+{
+public:
+    void* vftable;
+    std::map<SexyString, int> m_aliasToId;
+};
+
+class PlantNameMapper
+{
+public:
+    void* vftable;
+    std::map<SexyString, int> m_aliasToId;
+};
+
+// used for the custom id system
+std::vector<PlantType*> g_modPlantTypenames;
+std::vector<ZombieType*> g_modZombieTypenames;
+
+#define REGISTER_PLANT_TYPENAME(typename) \
+    g_modPlantTypenames.push_back(typename); \
+
+#define REGISTER_ZOMBIE_TYPENAME(typename) \
+    g_modZombieTypenames.push_back(typename); \
+
+typedef void* (*plantTypeCtor)(PlantType*);
+plantTypeCtor oPlantTypeCtor = NULL;
+
+void* hkPlantTypeCtor(PlantType* self)
+{
+    REGISTER_PLANT_TYPENAME(self);
+    return oPlantTypeCtor(self);
+}
+
+typedef PlantNameMapper* (*PlantNameMapperCtor)(PlantNameMapper*);
+PlantNameMapperCtor oPlantNameMapperCtor = NULL;
+
+void* hkCreatePlantNameMapper(PlantNameMapper* self)
+{
+    PlantNameMapper* obj = oPlantNameMapperCtor(self);
+    obj->m_aliasToId.clear();
+
+    for (size_t iter = 0; iter < g_modPlantTypenames.size(); iter++)
+    {
+        PlantType* type = g_modPlantTypenames[iter];
+        obj->m_aliasToId[type->TypeName] = type->IntegerID;
+    }
+
+    return obj;
+}
+
+typedef void* (*zombieTypeCtor)(ZombieType*);
+zombieTypeCtor oZombieTypeCtor = NULL;
+
+void* hkZombieTypeCtor(ZombieType* self)
+{
+    REGISTER_ZOMBIE_TYPENAME(self);
+    return oZombieTypeCtor(self);
+}
+
+typedef ZombieAlmanac* (*ZombieAlmanacCtor)(ZombieAlmanac*);
+ZombieAlmanacCtor oZombieAlmanacCtor = NULL;
+
+void* hkCreateZombieTypenameMap(ZombieAlmanac* self)
+{
+    ZombieAlmanac* obj = oZombieAlmanacCtor(self);
+    obj->m_aliasToId.clear();
+
+    for (size_t iter = 0; iter < g_modZombieTypenames.size(); iter++)
+    {
+        auto* type = g_modZombieTypenames[iter];
+        obj->m_aliasToId[type->TypeName] = type->IntegerID;
+    }
+
+    return obj;
+}
+
+#pragma endregion
+
+#pragma region Mummy Memory Fix
+
+typedef void(*camelMinigameModuleFunc)(int, int, int);
+camelMinigameModuleFunc cmmFunc = (camelMinigameModuleFunc)getActualOffset(0x78CFA0);
+
+void hkCamelZombieFunc(int a1, int a2, int a3)
+{
+    // Redirect call to some function in CamelMinigameModule
+    // This fixes the crash when camels are rising from the ground
+    cmmFunc(a1, a2, a3);
+}
+
+#pragma endregion
+
+typedef int(*worldMapDoMovement)(void*, float, float, bool);
+worldMapDoMovement oWorldMapDoMovement = NULL;
+
+int hkWorldMapDoMovement(void* self, float fX, float fY, bool allowVerticalMovement)
+{
+    return oWorldMapDoMovement(self, fX, fY, true);
+}
+
+#pragma endregion
+
+#pragma region Piano Zombie List
+
+typedef bool (*initZombiePianoList)(int, int);
+initZombiePianoList oInitZombiePianoList = NULL;
+
+std::vector<SexyString>* g_pianoList = NULL;
+bool g_pianoListInitialized = false;
+
+bool hkInitZombiePianoList(int a1, int a2)
+{
+    // This function is called every frame when a piano zombie is on screen
+    // So this global bool is needed to prevent wasting a massive amount of cpu time
+    if (!g_pianoListInitialized)
+    {
+        bool orig = oInitZombiePianoList(a1, a2);
+
+        uint ptrAddr = getActualOffset(0x1D890F4); // address of piano zombie's list in memory
+        g_pianoList = reinterpret_cast<std::vector<SexyString>*>(ptrAddr);
+
+        // @todo: add this to piano zombie's props instead?
+        g_pianoList->clear();
+        g_pianoList->push_back("cowboy");
+        g_pianoList->push_back("cowboy_armor1");
+        g_pianoList->push_back("cowboy_armor2");
+        g_pianoList->push_back("cowboy_armor4");
+        g_pianoList->push_back("cowboy_gargantuar");
+
+        g_pianoListInitialized = true;
+    }
+    return oInitZombiePianoList(a1, a2);
+}
+
+#pragma endregion
+
+typedef int(*boardCtor)(Board*, int*);
+boardCtor oBoardDraw = NULL;
+
+int hkBoardDraw(Board* self, int* a2)
+{
+
+    oBoardDraw(self, a2);
+    return 1;
+}
+
+typedef int (*mGetBoard)();
+mGetBoard oGetBoard = NULL;
+
+Board* hkGetBoard() {
+    // just making this available for own use
+    return (Board*)oGetBoard();
+}
+
+Board* getBoard() {
+    return hkGetBoard();
+}
+
+#pragma region Build Symbol Funcs
+
+Reflection::CRefManualSymbolBuilder::BuildSymbolsFunc PlantType::oPlantTypeBuildSymbols = nullptr;
+Reflection::CRefManualSymbolBuilder::ConstructFunc PlantType::oPlantTypeConstruct = nullptr;
+Reflection::CRefManualSymbolBuilder::BuildSymbolsFunc ZombieType::oZombieTypeBuildSymbols = nullptr;
+Reflection::CRefManualSymbolBuilder::ConstructFunc ZombieType::oZombieTypeConstruct = nullptr;
+
+#pragma endregion
+
+// hardcoded bull codename: 75667C
+
+__attribute__((constructor))
+// This is automatically executed when the lib is loaded
+// Run your initialization code here
+void libPVZ2ExpansionMod_main()
+{
+	LOGI("Initializing %s", LIB_TAG);
+
+    // Function hooks
+    FluffyHookFunction(0x8D3150, (void*)hkPlantTypeCtor, (void**)&oPlantTypeCtor);
+    FluffyHookFunction(0xDA5C58, (void*)hkCreatePlantNameMapper, (void**)&oPlantNameMapperCtor);
+    FluffyHookFunction(0xCA5768, (void*)hkZombieTypeCtor, (void**)&oZombieTypeCtor);
+    FluffyHookFunction(0x10643E0, (void*)hkCreateZombieTypenameMap, (void**)&oZombieAlmanacCtor);
+    FluffyHookFunction(0x8D3150, (void*)PlantType::construct, (void**)&PlantType::oPlantTypeConstruct);
+    FluffyHookFunction(0x8D1FE8, (void*)PlantType::buildSymbols, (void**)&PlantType::oPlantTypeBuildSymbols);
+    FluffyHookFunction(0xCA5768, (void*)ZombieType::construct, (void**)&ZombieType::oZombieTypeConstruct);
+    FluffyHookFunction(0xCA5894, (void*)ZombieType::buildSymbols, (void**)&ZombieType::oZombieTypeBuildSymbols);
+    FluffyHookFunction(0x789DC8, (void*)hkCamelZombieFunc, nullptr);
+    FluffyHookFunction(0x441068, (void*)hkWorldMapDoMovement, (void**)&oWorldMapDoMovement);
+    //FluffyHookFunction(0x9EC540, (void*)hkWorldDataCtor, (void**)&oWorldDataCtor);
+    FluffyHookFunction(0x885F80, (void*)hkInitZombiePianoList, (void**)&oInitZombiePianoList);
+
+    //FluffyHookFunction(0x72C56C, (void*)hkBoardDraw, (void**)&oBoardDraw);
+    //FluffyHookFunction(0x949EFC, (void*)hkGetBoard, (void**)&oGetBoard);
+
+    ZombieBullProps::modInit();
+    ZombieBullVeteranProps::modInit();
+    ZombieZcorpRacerProps::modInit();
+    ZombieEightiesArcadeProps::modInit();
+
+    //ZombieCamel::modInit();
+
+    LOGI("Finished initializing");
+}
--- README.md
+++ README.md
@@ -1,34 +1,37 @@
-# Felipe-s-Lib
-Okay, here's my contribution: This library includes: PVZDB for Thymed Events (LTE) to work Worldmap Vertical Movement Everything included in the RenoJson library and the Blazey library (I give them credit for their contributions)
+# **PvZ2 Libbing Stuff For 9.6**
 
-# PVZDB
-Includes the offsets for current versions so that LTE works; do not modify anything.
+# Credits
 
-# Worldmap Vertical Movement
-If you want, you can remove it simply by deleting the code and the function.
+* Blazey - original template
+* Brekko/Jay - original repositories (also thanks for helping me on softcode the Bull and fixing them)
+* Snowie and Vi - helping Blazey making original template
+
+# What is this?
+
+An expanded library for PvZ2 (9.6.1), based on Brekko's ExampleMod. I want it to be fully open sourced and public. My main goal is try to softcode any zombies via reflection system, which adds new properties to packages. It's currently in development, and no ARM64 version is available at the moment. 
+
+# Current Features
+* IntegerID backport! - Plant/Zombie types now support id mapping, which will help modders who dont want to constantly update the apk.
+* Arcade Pusher GI softcode
+* Mummy Memory from the original Example Mod
+* Stable softcode for Bull and Chair Racer, now it's shouldn't have any bug anymore for now. Also, now you can use the bull softcode for veteran bull too. Check the sample.json file in packages to learn how to use them.
+
+# Future Plans
+- Port my stuff to 64 bit once there are 64 bit template
 
 # Build Instructions
+Building this repo is the same as building Blazey example mod update. The instructions regarding that [you can find here](https://github.com/BlazeyLol/PVZ2ExampleMod) and [here](https://github.com/BlazeyLol/PVZ2ExpansionMod) or [in this old doc](https://docs.google.com/document/d/1egwNqNJjw61MlLYMgzUewdTkztpB-6VruHrSez90gcA/edit?usp=sharing).
+This library requires some packages modifications in order to work. You can find example Plant/Zombie types jsons in the Packages directory.
 
-Building this repo is the same as building Blazey example mod update. The instructions regarding that you can find here and here or in this old doc. This library requires some packages modifications in order to work. You can find example Plant/Zombie types jsons in the Packages directory. If you want to utilize Power Lily's custom fields, add these to her props:
-
-CollectableType (string)
-CollectableCount (integer)
-CollectableOffsetY (float)
-Arcade's new property is GridItemType (string). One important note: DO NOT use a grid item which class isn't GridItemEightiesArcadeCabinet, otherwise zombie won't be able to push it.
+Arcade's new property is `GridItemType` (string). One important note: DO NOT use a grid item which class isn't `GridItemEightiesArcadeCabinet`, otherwise zombie won't be able to push it.
 
 # Bull's and Veteran Bull's new property:
-
-RiderType (string) (using this for bull props)
-VetRiderType (string) (using this for veteran bull props)
-RiderLayerToHide (string list) (this can use for both props)
-LaunchDistance (float) (this can use for both props)
-NOTE: The RiderType and VetRiderType must use ZombieImp class with BullRiderProps. If you want using any vanilla props of them, just not use them as the function will using the hardcode value instead.
+- RiderType (string) (using this for bull props) 
+- VetRiderType (string) (using this for veteran bull props)
+- RiderLayerToHide (string list) (this can use for both props)
+- LaunchDistance (float) (this can use for both props)
+- NOTE: The RiderType and VetRiderType must use ZombieImp class with BullRiderProps. If you want using any vanilla props of them, just not use them as the function will using the hardcode value instead.
 
 # ZcorpRacer's (Chair Racer) new property:
-
-RacerType (string)
-NOTE: The RacerType must use ZcorpRacerZombie class. If you want using any vanilla props of them, just not use them as the function will using the hardcode value instead.
-
-
-
-
+- RacerType (string)
+- NOTE: Beside the RacerType must use ZcorpRacerZombie class. If you want using any vanilla props of them, just not use them as the function will using the hardcode value instead.
